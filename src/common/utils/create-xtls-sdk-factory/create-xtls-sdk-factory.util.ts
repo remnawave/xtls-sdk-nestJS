0f07f491f80955d6aa59808f7aafab93ec719ab6
ivanChertan <thechertan@yandex.ru>
2024-11-16 00:24:07 +0300

    fix: version

--- src/common/utils/create-xtls-sdk-factory/create-xtls-sdk-factory.util.ts
+++ src/common/utils/create-xtls-sdk-factory/create-xtls-sdk-factory.util.ts
@@ -1,7 +1,7 @@
 import { XtlsApi } from '@remnawave/xtls-sdk';
 import { Logger } from '@nestjs/common';
 import { IXtlsConfig } from './interfaces';
-const logger = new Logger('nestjs-grammy:create-bot-factory.util');
+const logger = new Logger('xtls-sdk.util');
 
 export function createXtlsSdkFactory(moduleOptions: IXtlsConfig): XtlsApi {
     const xtlsApi = new XtlsApi(moduleOptions.ip, moduleOptions.port);
